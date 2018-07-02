14a47fd0c14337cd5df57dbc90f1535bef26f53a
Mate Kovacs <Mate.Kovacs@softwire.com>
2018-07-02 11:54:11 +0100

    added method that refreshes results periodically

--- BusBoard/assembleResult.js
+++ BusBoard/assembleResult.js
@@ -2,11 +2,9 @@ module.exports = function (info, sortedStops)
 {
     var result = [];
     for(i = 0; i<info.length; i++){
-        console.log(info[i].body);
         let sorted = info[i].body.sort(function(a,b){
             return(a.timeToStation -b.timeToStation);
         });
-        //console.log(sorted);
         let buses = [];
         let error = sorted.length === 0 ? {status: 404, message: 'Live arrival times unavailable.'} :  {status: 200, message: 'OK'};
         for(let j = 0; j<5 && j<sorted.length; j++){
